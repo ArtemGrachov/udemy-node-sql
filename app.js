500921379068edc2e2d7584abef7a1488cb9d1d3
Artem Grachov <grachov2195@gmail.com>
2019-01-25 10:50:23 +0200

    404 page

--- app.js
+++ app.js
@@ -13,4 +13,10 @@ app.use(bodyParser.urlencoded({
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res) => {
+  res
+    .status(404)
+    .send('<h1>Page not found</h1>');
+});
+
 app.listen(3000);

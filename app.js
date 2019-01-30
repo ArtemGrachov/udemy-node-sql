6d52ecb7f94e2647c98cacfb6b2911df9f8ec6dd
Artem Grachov <grachov2195@gmail.com>
2019-01-30 17:15:36 +0200

    User middleware

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const
   bodyParser = require('body-parser');
 
 const app = express();
-const sequelize = require('./util/database');
 
 app.set('view engine', 'ejs');
 
@@ -18,12 +17,44 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById(1)
+    .then(user => {
+      res.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
-  .catch(err => console.log(err));
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
-app.listen(3000);
+Product.belongsTo(User, {
+  constraints: true,
+  onDelete: 'CASCADE'
+});
+User.hasMany(Product);
+
+
+sequelize.sync()
+  .then(result => {
+    User
+      .findById(1)
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({
+        name: 'User',
+        email: 'user@user.com'
+      })
+    }
+    return user;
+  })
+  .then(() => app.listen(3000))
+  .catch(err => console.log(err));

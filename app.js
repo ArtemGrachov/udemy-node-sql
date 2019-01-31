b7dd1d9ace60c195692dcfe0475c46304cc6e65d
Artem Grachov <grachov2195@gmail.com>
2019-01-31 10:51:50 +0200

    Order models

--- app.js
+++ app.js
@@ -36,6 +36,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 Product.belongsTo(User, {
   constraints: true,
@@ -50,7 +52,11 @@ Cart.belongsToMany(Product, {
 Product.belongsToMany(Cart, {
   through: CartItem
 });
-
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {
+  through: OrderItem
+});
 
 sequelize.sync()
   .then(result => {

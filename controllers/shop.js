dccf8c4182f0d8b1f813dfb05b178ea0a2ebe5a0
Artem Grachov <grachov2195@gmail.com>
2019-01-31 10:43:39 +0200

    Deleting products from cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -106,10 +106,23 @@ exports.postCart = (req, res) => {
 
 exports.postCartDeleteProduct = (req, res) => {
   const productId = req.body.productId;
-  Product.findById(productId, product => {
-    Cart.deleteProduct(productId, product.price);
-    res.redirect('/cart');
-  })
+  console.log(productId)
+  req
+    .user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts({
+        where: {
+          id: productId
+        }
+      });
+    })
+    .then(products => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then(result => res.redirect('/cart'))
+    .catch(err => console.log(err))
 }
 
 exports.getOrders = (req, res) => {

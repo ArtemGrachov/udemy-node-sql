72c1249e6a80b6eecf571df533b3dde7264b3981
Artem Grachov <grachov2195@gmail.com>
2019-01-31 15:28:52 +0200

    Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,5 @@
 const
-  Product = require('../models/product'),
-  Cart = require('../models/cart');
+  Product = require('../models/product');
 
 exports.getProducts = (req, res) => {
   Product
@@ -125,9 +124,48 @@ exports.postCartDeleteProduct = (req, res) => {
     .catch(err => console.log(err))
 }
 
+exports.postOrder = (req, res) => {
+  let fetchedCart;
+  req
+    .user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts();
+    })
+    .then(products => {
+      return req
+        .user
+        .createOrder()
+        .then(order => {
+          return order.addProducts(
+            products.map(
+              product => {
+                product.orderItem = {
+                  quantity: product.cartItem.quantity
+                };
+                return product;
+              }
+            )
+          );
+        })
+        .catch(err => console.log(err));
+    })
+    .then(result => fetchedCart.setProducts(null))
+    .then(result => res.redirect('/orders'))
+    .catch(err => console.log(err))
+}
+
 exports.getOrders = (req, res) => {
-  res.render('shop/orders', {
-    pageTitle: 'Orders',
-    path: 'shop/orders'
-  });
+  req
+    .user
+    .getOrders({ include: ['products'] })
+    .then(orders => {
+      res.render('shop/orders', {
+        pageTitle: 'Orders',
+        path: 'shop/orders',
+        orders: orders
+      });
+    })
+    .catch(err => console.log(err));
 };
